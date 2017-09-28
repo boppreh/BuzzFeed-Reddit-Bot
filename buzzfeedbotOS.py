535d2a8f50efcf7bfaa50ca69d8ebdb8c6ffeaeb
BoppreH <boppreh@gmail.com>
2017-09-27 23:13:12 -0300

    General refactoring of code

--- buzzfeedbotOS.py
+++ buzzfeedbotOS.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from langdetect import detect, lang_detect_exception
+import pytz
 import datetime
 import time
 import requests
@@ -19,45 +20,25 @@ def reddit_bot(headline, main_text, link):
 '''Checks if there are numbered bullet points in the article. If there is at least one, returns true, 
 if there are none, returns false''' 
 def check_for_numbered_points(link_to_check):
-	i=0
 	session = requests.Session()
 	clickbait_article = session.get(link_to_check)
 	soup = BeautifulSoup(clickbait_article.content, 'html.parser')
-	for title in soup.find_all('h3'):
-		for number in title.find_all('span', attrs={'class': 'subbuzz__number'}):
-			i+=1
-	if i > 0:
-		return (True)
-	else:
-		return (False)
+	return any(title.find_all('span', attrs={'class': 'subbuzz__number'}) for title in soup.find_all('h3'))
 		
 '''Gets current time in EST'''
 def current_time_eastern():
-	from time import gmtime, strftime
-	time_diff = 4
-	gm_time = strftime("%#H:%M:%S", gmtime())
-	gm_hour, min_secs =  gm_time.split(':', 1)
-	if int(gm_hour)>= time_diff:
-		est_hour = str(int(gm_hour)-time_diff)
-	else:
-		est_hour = str(int(gm_hour)+24-time_diff)
-	es_time = est_hour + ':'+ min_secs
-	return es_time
+	now_est = datetime.now(pytz.timezone('EST'))
+	return '{}:{}:{}'.format(now_est.hour, now_est.minute, now_est.second)
 	
 '''Checks if the post has already been submitted 
 Returns True if post was submitted already and returns False otherwise'''
 def post_made_check(post_title):
-	post_made = False
 	reddit = praw.Reddit(client_id='',
 					client_secret= '',
 					user_agent='BuzzFeed bot')
 	subreddit = reddit.subreddit('buzzfeedbot')
 	submissions = subreddit.new(limit=40)
-	for submission in submissions:
-		if submission.title == post_title:
-			post_made = True
-			break
-	return (post_made)
+	return any(submission.title == post_title for submission in submissions)
 
 '''Gets the link to the article that will be posted on the sub.
 The two if-statements below check if (1) the aretile starts with a number, (2) the post hasn't been made already,
@@ -72,33 +53,34 @@ def article_info(date, error):
 	soup = BeautifulSoup(daily_archive.content, 'html.parser')
 	for article_to_open in soup.find_all('li', attrs={'class': 'bf_dom'}):
 		#End script if post was already made longer than one hour ago
-		post_made = post_made_check(article_to_open.text)
-		if post_made == True:
-			if error == True:
+		if post_made_check(article_to_open.text):
+			if error:
 				continue
-			elif error == False:
+			else:
 				break
 		lc_art_title = article_to_open.text.lower()
+		
 		try:
-			if (article_to_open.text[0].isdigit() or article_to_open.text.lower().startswith('top')) \
-			and detect(article_to_open.text) == 'en' and not any(words in lc_art_title for words in break_words):
-	
-				no_of_points = [int(s) for s in article_to_open.text.split() if s.isdigit()] #Records number of points in the article 
-				for link in article_to_open.find_all('a', href=True):
-					top_x_link = 'https://www.buzzfeed.com' + link['href']
-					try: #Avoids rare case of when there is an index error (occurs when article starts with number immediately followed by a symbol)
-						article_text_to_use = clickbait_meat(top_x_link, no_of_points[0])
-						if article_text_to_use == '':
-							break
-						else:
-							reddit_bot(article_to_open.text, article_text_to_use, top_x_link)
-							print(article_to_open.text)
-							time.sleep(1)
-							break
-					except IndexError:
-						break
+			is_english = detect(article_to_open.text) == 'en'
 		except lang_detect_exception.LangDetectException:
 			continue
+		is_listicle = lc_art_title[0].isdigit() or lc_art_title.startswith('top')
+		has_banned_keyword = any(words in lc_art_title for words in break_words)
+		
+		if is_listicle and is_english and not has_banned_keyword:
+			try:
+				no_of_points = next(int(s) for s in article_to_open.text.split() if s.isdigit()) #Records number of points in the article 
+			except StopIteration:
+				break
+			
+			for link in article_to_open.find_all('a', href=True):
+				top_x_link = 'https://www.buzzfeed.com' + link['href']
+				article_text_to_use = clickbait_meat(top_x_link, no_of_points)
+				if article_text_to_use:
+					reddit_bot(article_to_open.text, article_text_to_use, top_x_link)
+					print(article_to_open.text)
+					time.sleep(1)
+				break
 
 '''Concatenates the main points of the article into a single string and also makes sure the string isn't empty.
 Also checks to make sure  the number of subpoints in the article is equal to the numbe rthe atricle title starts with'''			
@@ -115,38 +97,37 @@ def clickbait_meat(link_to_check, total_points):
 	for title in soup.find_all('h3'):
 		for number in title.find_all('span', attrs={'class': 'subbuzz__number'}):
 			bullet_point = True
-		if bullet_point == True or any_numbered_points == False:
+		if bullet_point or not any_numbered_points:
 			for article in title.find_all('span', attrs={'class': 'js-subbuzz__title-text'}):
 				if len(article.text)<4 or article.text.endswith(':'):
 					return ''
+				top_x_final_temp = top_x_final
+				if this_when_counter == 3:
+					this_when_counter = 0
+					return ''
+				if article.text.startswith('When') or article.text.startswith('This'):
+					this_when_counter += 1
 				else:
-					top_x_final_temp = top_x_final
-					if this_when_counter == 3:
-						this_when_counter = 0
-						return ''
-					if article.text.startswith('When') or article.text.startswith('This'):
-						this_when_counter += 1
-					else:
-						this_when_counter = 0
-					try:
-						for link in article.find_all('a', href=True):
-							if article.text.startswith(str(i)):
-								top_x_final += '[' + article.text +']('+ link['href']+')' + '\n'
-							else:
-								top_x_final += str(i) + '. [' + article.text +']('+ link['href']+')' + '\n'
-							break
-					except KeyError:
-						pass
-					if top_x_final_temp == top_x_final:
+					this_when_counter = 0
+				try:
+					for link in article.find_all('a', href=True):
 						if article.text.startswith(str(i)):
-							top_x_final += article.text  + '\n'
+							top_x_final += '[' + article.text +']('+ link['href']+')' + '\n'
 						else:
-							top_x_final += str(i) + '. '+ article.text  + '\n'
+							top_x_final += str(i) + '. [' + article.text +']('+ link['href']+')' + '\n'
+						break
+				except KeyError:
+					pass
+				if top_x_final_temp == top_x_final:
+					if article.text.startswith(str(i)):
+						top_x_final += article.text  + '\n'
+					else:
+						top_x_final += str(i) + '. '+ article.text  + '\n'
 				i+=1
 		bullet_point = False
 	if total_points != i-1:
 		top_x_final = ''
-	return(top_x_final)
+	return top_x_final
 
 if __name__ == "__main__":
 	error_occured = False
